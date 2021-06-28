7a4f72ce310512af01496b364a90f550644d02df
Kumi <64310263+KumiGhardt@users.noreply.github.com>
2021-06-28 20:37:07 +0200

    this commit adds a console.log pushing to docker

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.get("/", (req,res) => {
     res.send("<H1>Hello world from Kumi!!!</H1>")
-    console.log("It is happening")
+    console.log("Pushing to Docker hub")
 });
 
 const port = process.env.PORT || 3000;
